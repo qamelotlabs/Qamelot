5657b9ad7e5b08e634646190c731d92982033ad3
sourav <souravs@appycodes.com>
2022-06-20 14:00:25 +0530

    Separating user add functions and added public metrics check

--- TwitterApp/scraping_and_seeding/twitter_scraping.py
+++ TwitterApp/scraping_and_seeding/twitter_scraping.py
@@ -1,5 +1,5 @@
 from Qamelot.project_modules import *
-
+from datetime import date
 
 def fetch_twitter_public_metrics(id='1527749317402013696'):
     url = '{}tweets?ids={}&tweet.fields=public_metrics&expansions=attachments.media_keys&media.fields=public_metrics'.format(settings.TWITER_API_URL, id)
@@ -19,34 +19,32 @@ def fetch_user(username='twitterdev'):
 
 
 def fetch_user_recent_tweets(usernames='twitterdev', keyword='Platform'):
-    # user_details = fetch_user(username)
-    # if 'data' in user_details:
-    #     if len(user_details['data']) > 0:
-    #         create_user_data(user_details['data'][0])
-            objs = NFTCollection.objects.all()
-            influencer_names = ''
-            for o in objs:
-                print('collection_name: ', o.collectionName)
-                keyword = o.collectionName
-                url = '{}tweets/search/recent?query={} from:{}&max_results=10&expansions=author_id&tweet.fields=created_at,lang,conversation_id,in_reply_to_user_id,referenced_tweets&user.fields=created_at,entities'.format(settings.TWITER_API_URL, keyword, usernames)
-                headers = {'Authorization': 'Bearer {}'.format(
-                    settings.TWITTER_BEARER_TOKEN)}
-                res = requests.get(url, headers=headers)
-                twitterResData = res.json()
-                if 'data' in twitterResData:
-                    for data in twitterResData['data']:
-                        data['metrics'] = fetch_twitter_public_metrics(data['id'])
-                        if len(data) > 0:
-                            print(data)
-                            create_twitter_data(data)
-                        else:
-                            print('Tweet data not found1')
+    objs = NFTCollection.objects.all()
+    influencer_names = ''
+    for o in objs:
+        print('collection_name: ', o.collectionName) 
+        keyword = o.collectionName
+        url = '{}tweets/search/recent?query={} from:{}&max_results=10&expansions=author_id&tweet.fields=created_at,lang,conversation_id,in_reply_to_user_id,referenced_tweets&user.fields=created_at,entities'.format(settings.TWITER_API_URL, keyword, usernames)
+        headers = {'Authorization': 'Bearer {}'.format(
+            settings.TWITTER_BEARER_TOKEN)}
+        res = requests.get(url, headers=headers)
+        twitterResData = res.json()
+        if 'data' in twitterResData:
+            for data in twitterResData['data']:
+                data['metrics'] = fetch_twitter_public_metrics(data['id'])
+                data['collection_id'] = o.id
+                if len(data) > 0:
+                    print(data)
+                    create_twitter_data(data)
                 else:
-                    print('Tweet data not found2')
-    #     else:
-    #         print('User data not available')
-    # else:
-    #     print('User data not available')
+                    print('Tweet data not found1')
+            collection_seed_status = CollectionSeedStatus.objects.create(
+                collection_id=o.id,
+                status=True,
+                created_at=str(date.today())
+            )
+        else:
+            print('Tweet data not found2')
 
 def create_user_data(data):
 
@@ -72,40 +70,43 @@ def create_user_data(data):
 def create_twitter_data(data):
     tweet_detail = TweetData.objects.filter(id=data['id']).exists()
     if tweet_detail is False:
-
-        tweetDetail = TweetData.objects.create(
-            id=data['id'],
-            author_id=data['author_id'],
-            lang=data['lang'],
-            retweet_count=data['metrics']['data'][0]['public_metrics']['retweet_count'],
-            reply_count=data['metrics']['data'][0]['public_metrics']['reply_count'],
-            like_count=data['metrics']['data'][0]['public_metrics']['like_count'],
-            quote_count=data['metrics']['data'][0]['public_metrics']['quote_count'],
-            twitter_text=data['metrics']['data'][0]['text'],
-            # in_reply_to_user_id=data['in_reply_to_user_id'],
-            # referenced_tweets=data['referenced_tweets'],
-            # mentions              = ''
-        )
-        if 'in_reply_to_user_id' in data:
-            TweetData.objects.filter(id__exact=tweetDetail.id).update(
-                in_reply_to_user_id=data['in_reply_to_user_id'],
-                referenced_tweets=data['referenced_tweets']
+        if 'data' in data['metrics']:
+            tweetDetail = TweetData.objects.create(
+                id=data['id'],
+                author_id=data['author_id'],
+                lang=data['lang'],
+                retweet_count=data['metrics']['data'][0]['public_metrics']['retweet_count'],
+                reply_count=data['metrics']['data'][0]['public_metrics']['reply_count'],
+                like_count=data['metrics']['data'][0]['public_metrics']['like_count'],
+                quote_count=data['metrics']['data'][0]['public_metrics']['quote_count'],
+                twitter_text=data['metrics']['data'][0]['text'],
+                collection_id=data['collection_id'],
+                # in_reply_to_user_id=data['in_reply_to_user_id'],
+                # referenced_tweets=data['referenced_tweets'],
+                # mentions              = ''
             )
+            if 'in_reply_to_user_id' in data:
+                TweetData.objects.filter(id__exact=tweetDetail.id).update(
+                    in_reply_to_user_id=data['in_reply_to_user_id']
+                )
+            if 'referenced_tweets' in data:
+                TweetData.objects.filter(id__exact=tweetDetail.id).update(
+                    referenced_tweets=data['referenced_tweets']
+                )
+        else:
+            print('Twitter public metrics not available.')
 
-    else:
-        if 'in_reply_to_user_id' in data:
-            TweetData.objects.filter(id__exact=data['id']).update(
-                in_reply_to_user_id=data['in_reply_to_user_id'],
-                referenced_tweets=data['referenced_tweets'],
-            )
-            print('updated')
 
 def fetch_influencers():
     objs = NftInfluencers.objects.all()
     influencer_names = ''
     for o in objs:
         print('name: ', o.name)
-        influencer_names += o.name+' OR '
+        user_details = fetch_user(o.name)
+        if 'data' in user_details:
+            if len(user_details['data']) > 0:
+                create_user_data(user_details['data'][0])
+                influencer_names += o.name+' OR '
     influencer_names = influencer_names[:-4]
     print('----'+influencer_names)
     fetch_user_recent_tweets(influencer_names)    
