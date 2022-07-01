1dd21b635eb0773c75f68aca1f9662646f1e0717
Dipankar Chowdhury <dipankarchoudhury.28.dc@gmail.com>
2022-07-01 15:38:09 +0530

    modifed qamelot main url file

--- Qamelot/urls.py
+++ Qamelot/urls.py
@@ -8,10 +8,14 @@ from TwitterApp.scraping_and_seeding.twitter_scraping import *
 from NFTCollectionApp.Seeding.CollectionsSeeding import *
 from dataScience.views import *
 # from .scheduler import *
+from rest_framework.documentation import include_docs_urls
+from django.conf.urls import url
+
 
 urlpatterns = [
     # path('graphql', GraphQLView.as_view(graphiql=True)),
 
+    url(r'^', include_docs_urls(title='Qamelot API Documentations')),
     path('admin/', admin.site.urls),
     path('',include('WebAPI.urls')),
     path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
